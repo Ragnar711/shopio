687d33f484429e53882add53a95a95ce508b4b97
Ragnar711 <azizbechaib711@gmail.com>
2023-09-05 11:19:52 +0100

    save

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const Product = ({ product }) => {
+    return <Card className="my-3 py-3 rounded"></Card>;
+};
+
+export default Product;
