5b124a8276ad3fe9ab9daf6998e56a4f4ff39b3c
Ragnar711 <azizbechaib711@gmail.com>
2023-09-12 14:02:19 +0100

    Implemented react router

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
     const cardStyle = {
@@ -13,20 +14,20 @@ const Product = ({ product }) => {
     };
     return (
         <Card className="my-3 p-3 rounded" style={cardStyle}>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img
                     src={product.image}
                     variant="top"
                     height="300px"
                     width="300px"
                 />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as="div" style={titleStyle}>
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
                 <Card.Text as="h3">${product.price}</Card.Text>
             </Card.Body>
         </Card>
