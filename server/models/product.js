4eb27191891a94cf400919dacebe72f05803a126
Ragnar711 <azizbechaib711@gmail.com>
2023-09-14 12:10:24 +0100

    Seeding data

--- server/models/product.js
+++ server/models/product.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
 const productSchema = new mongoose.Schema(
     {
         user: {
